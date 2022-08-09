e7c43f2c82bd10b7209972c1082a1bdbb2df78b3
Allison Ause <allison.ause@gmail.com>
2022-08-09 16:00:56 -0700

    fully functional route for deleting / logging out user

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -2,6 +2,20 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
+
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? testUser.password;
+  const agent = request.agent(app);
+  const user = await UserService.create({ ...testUser, ...userProps });
+
+  const { email } = user;
+  await agent.post('/api/v1/users/sessions').send({
+    email,
+    password,
+  });
+  return [agent, user];
+};
 
 const testUser = {
   email: 'test@test.com',
@@ -29,6 +43,12 @@ describe('/api/v1/users routes', () => {
       .send({ email: 'test@test.com', password: '1234password' });
     expect(res.status).toBe(200);
     expect(res.body.message).toBe('Successful Login!');
+  });
+
+  it('#DELETE /sessions logs out user', async () => {
+    const [agent] = await registerAndLogin();
+    const res = await agent.delete('/api/v1/users/sessions');
+    expect(res.body.message).toBe('Successfully Logged Out!');
   });
 
   afterAll(() => {
